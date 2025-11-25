a9e8b5a4b6342b94e3db4cfa66cd554fdf31f8fa
RadhikaSuryawanshi <radhikasuryawanshi279@gmail.com>
2025-11-25 20:53:30 +0530

    Created Home page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage(){
+    return(
+        <div className="w-full h-screen bg-amber-200 p-1 flex justify-center ">
+            <div className="w-[70%] h-full text-center ">
+             <h1 className="font-extrabold text-3xl ">25, M.Sc. Computer Science (2025) — I'm a Full Stack Developer and Design Engineer enthusiast with a passion for building seamless, high-performance user interfaces. Currently exploring the idea of creating a fast-growing, exclusive UI component library focused on aesthetics, usability, and speed. I care deeply about how interfaces look, feel, and most importantly, function. </h1>
+             </div>
+        </div>
+    )
+}
